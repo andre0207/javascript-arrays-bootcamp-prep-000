9f295c152673bbaf674fcde3529eb75db72c914f
Andre Pierre <andre.laurence94@gmail.com>
2020-04-23 20:36:04 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -15,11 +15,5 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   return array
 }
 
-function addElementToEndOfArray(array, element) {
-  array.push(element);
-  return array
-}
 
-function addElementToEndOfArray(array, element) {
-  var endArray = [...array, element]
 }
